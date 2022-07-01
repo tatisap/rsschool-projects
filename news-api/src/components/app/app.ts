c5d7311f9930aa53a531977bd4a5d6e5bf8ac323
Tatiana Sapun <tatiana.sapun@gmail.com>
2022-07-01 14:44:50 +0300

    feat: cover code with types in app.ts

--- news-api/src/components/app/app.ts
+++ news-api/src/components/app/app.ts
@@ -1,5 +1,6 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
+import { ArticlesResponse, SourcesResponse } from '../../types';
 
 class App {
     private controller: AppController;
@@ -11,10 +12,10 @@ class App {
     }
 
     public start() {
-        (document.querySelector('.sources') as HTMLDivElement).addEventListener('click', (e) =>
-            this.controller.getNews(e, (data) => this.view.drawNews(data))
+        (document.querySelector('.sources') as HTMLDivElement).addEventListener('click', (e: MouseEvent) =>
+            this.controller.getNews(e, (data: ArticlesResponse | undefined) => this.view.drawNews(data))
         );
-        this.controller.getSources((data) => this.view.drawSources(data));
+        this.controller.getSources((data: SourcesResponse | undefined) => this.view.drawSources(data));
     }
 }
 
