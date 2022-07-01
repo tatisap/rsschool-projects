a8c1111b92d7021d5cde8e396d8bce2a53487ee8
Tatiana Sapun <tatiana.sapun@gmail.com>
2022-07-01 14:49:56 +0300

    feat: cover code with types in controller.ts

--- news-api/src/components/controller/controller.ts
+++ news-api/src/components/controller/controller.ts
@@ -2,7 +2,7 @@ import { ArticlesResponse, Drawer, SourcesResponse } from '../../types';
 import AppLoader from './appLoader';
 
 class AppController extends AppLoader {
-    public getSources(callback: Drawer<SourcesResponse>) {
+    public getSources(callback: Drawer<SourcesResponse>): void {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -11,7 +11,7 @@ class AppController extends AppLoader {
         );
     }
 
-    public getNews(e: Event, callback: Drawer<ArticlesResponse>) {
+    public getNews(e: Event, callback: Drawer<ArticlesResponse>): void {
         let target = e.target as HTMLElement | null;
         const newsContainer = e.currentTarget as HTMLElement | null;
         if (newsContainer === null) return;
