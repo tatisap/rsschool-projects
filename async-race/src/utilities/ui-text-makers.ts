e808609bb5dc53f35f2fee2f3ab0fc03a01eab27
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-08-04 10:44:54 +0200

    feat: add text-makers

---
+++ async-race/src/utilities/ui-text-makers.ts
@@ -0,0 +1,4 @@
+export const createSectionTitleText = (text: string, totalAmount: string): string =>
+  `${text}(${totalAmount})`;
+
+export const createPageNumberText = (currentValue: number): string => `Page #${currentValue}`;
