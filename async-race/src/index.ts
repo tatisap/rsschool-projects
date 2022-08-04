39309109f29fbf0ac2286a58594b8128f27f3e42
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-08-04 18:00:22 +0200

    feat: implement starting app

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,4 +1,23 @@
 import './style.scss';
-import { renderMainPage } from './ui/user-interface';
+import renderPage from './ui/render-page';
+import { Numbers } from './types/enums';
+import API from './api/api';
+import { MAX_CARS_PER_PAGE, MAX_WINNERS_PER_PAGE } from './constants/constants';
+import { Car, Info, Winner } from './types/types';
+import store from './store/store';
 
-renderMainPage();
+const getPageInfo = async (): Promise<[Info<Car>, Info<Winner>]> => {
+  const carsInfo: Info<Car> = await API.getCars({
+    _page: Numbers.One,
+    _limit: MAX_CARS_PER_PAGE,
+  });
+  const winnersInfo: Info<Winner> = await API.getWinners({
+    _page: Numbers.One,
+    _limit: MAX_WINNERS_PER_PAGE,
+  });
+  [store.cars, store.carsAmount] = [carsInfo.content, Number(carsInfo.totalAmount)];
+  [store.winners, store.winnersAmount] = [winnersInfo.content, Number(winnersInfo.totalAmount)];
+  return [carsInfo, winnersInfo];
+};
+
+getPageInfo().then(renderPage);
