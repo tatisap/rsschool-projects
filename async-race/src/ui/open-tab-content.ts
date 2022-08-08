ea58cda5c04f063318ce2867d00af6cd61c89b5e
Tatsiana Sapun <tatiana.sapun@gmail.com>
2022-08-08 15:46:22 +0300

    fix: add types to open-tab function

--- async-race/src/ui/open-tab-content.ts
+++ async-race/src/ui/open-tab-content.ts
@@ -1,14 +1,13 @@
 export default (event: Event): void => {
   if (!(event.target as HTMLElement).classList.contains('tab-button')) return;
-  const tab = event.target as HTMLButtonElement;
+  const tab: HTMLButtonElement = event.target as HTMLButtonElement;
   (document.querySelectorAll('.tab-button') as NodeListOf<HTMLButtonElement>).forEach(
-    (tabButton: HTMLButtonElement) => tabButton.classList.remove('tab-button_active')
+    (tabButton: HTMLButtonElement): void => tabButton.classList.remove('tab-button_active')
   );
   tab.classList.add('tab-button_active');
   (document.querySelectorAll('section') as NodeListOf<HTMLElement>).forEach(
-    (tabContent: HTMLElement) => {
-      const content: HTMLElement = tabContent;
-      content.classList.remove('section_visible');
+    (tabContent: HTMLElement): void => {
+      tabContent.classList.remove('section_visible');
     }
   );
   (document.getElementById(tab.textContent?.toLowerCase() as string) as HTMLElement).classList.add(
